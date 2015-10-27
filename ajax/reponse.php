b827809cf9006755fbcbc2050866ea8f2bb90377
David ARCOLE <davidar02300@gmail.com>
2015-10-27 10:22:56 +0100

    sauvegarde du 27/10/2015

--- ajax/reponse.php
+++ ajax/reponse.php
@@ -2,5 +2,4 @@
 	$date = date("d-m-Y");
 	$heure = date("H:i");
 	echo"<span>Message de : ".$_POST['nom']."</span><p>Corps du message : ".$_POST['message']."</p><time>date :".$date." heure :".$heure."</time>";
-
  ?>
